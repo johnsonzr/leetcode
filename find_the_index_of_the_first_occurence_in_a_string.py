79402facfb96a264a51bb101922ea3fdac83e9e9
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2024-04-25 20:59:09 -0400

    Add files via upload

---
+++ find_the_index_of_the_first_occurence_in_a_string.py
@@ -0,0 +1,12 @@
+needle = 'sad'
+haystack = 'sadbutsad'
+
+if needle not in haystack:
+    return -1
+if needle in haystack:
+    nl = len(needle)
+    hl = len(haystack)
+    for y in range(hl - nl + 1):
+        if needle in haystack[y:nl + y]:
+            return y
+                
