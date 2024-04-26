7bc071f805be1041703ff94ddae1ce8026aeeb16
Zac Johnson <52793864+johnsonzr@users.noreply.github.com>
2024-04-25 21:01:09 -0400

    Update find_the_index_of_the_first_occurence_in_a_string.py
    added leetcode stats

--- find_the_index_of_the_first_occurence_in_a_string.py
+++ find_the_index_of_the_first_occurence_in_a_string.py
@@ -9,4 +9,9 @@ if needle in haystack:
     for y in range(hl - nl + 1):
         if needle in haystack[y:nl + y]:
             return y
-                
+
+# Runtime:
+# Beats 98%
+
+# Memory:
+# Beats 90.4%
